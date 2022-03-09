6d757d1204f748c08c7f4beb2082d0d446d52324
Yuqi <wangyuqi2000@outlook.com>
2022-03-09 22:46:33 +0800

    DEL:proto scripts

--- session/sessionpb/gen.sh
+++
@@ -1,3 +0,0 @@
-#kitex -module raft  -type protobuf -service raftpb raft.proto
-protoc -I=.  -I=$GOPATH/pkg/mod -I=$GOPATH/pkg/mod/github.com/gogo/protobuf@v1.3.2/gogoproto --gofast_out=plugins=grpc:. session.proto
-
